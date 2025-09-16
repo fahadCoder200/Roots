eb5b09e231e7c7ea63b16b6abc232122cd69540f
fahadCoder200 <maazaka31@gmail.com>
2025-09-16 08:38:17 +0500

    Fix don again 3

--- src/app/create-teacher/page.tsx
+++ src/app/create-teacher/page.tsx
@@ -1,8 +1,9 @@
 import TeacherForm from "@/components/teacherForm";
+import {prisma} from "@/lib/prisma"
 
 export default async function CreateTeacher() {
 
-    const subjects = await prisma?.subject.findMany();
+    const subjects = await prisma.subject.findMany();
 
     return (
         <TeacherForm subjectList={subjects || []} />
