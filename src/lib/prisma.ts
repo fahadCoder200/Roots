184729c026309f1ce0e258ca81f057bc0e4ab8cd
fahadCoder200 <maazaka31@gmail.com>
2025-09-16 21:48:54 +0500

    Prisma error debug 4

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
+  console.log("DB URL:", process.env.DATABASE_URL);
   prisma = new PrismaClient({
     log: ["query", "info", "warn", "error"],
   });
