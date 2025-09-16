8ecc4fb3236ef9d23a64f2e3c46ee17fa6075914
fahadCoder200 <maazaka31@gmail.com>
2025-09-16 22:19:40 +0500

    About page added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import SideBar from "@/components/sidebar"
+
+export default function Page() {
+    return (
+        <>
+            <SideBar />
+            <p>This Awesome, Functional And Beautiful Website is created by a genius in making with motivational and moral support from his CS teacher! Fahad Khalid</p>
+        </>
+    )
+}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -15,7 +15,7 @@ export default function SideBar(){
                 <Link href="/dashboard-admin" onClick={() => setSelected("/dashboard-admin")} className={`hover:border-red-600 border-2 border-transparent px-4 py-2 rounded-lg cursor-pointer ${selected === '/dashboard-admin' && "bg-red-700 text-white"}`}>Home</Link>
                 <Link href="/students-info" onClick={() => setSelected("/students-info")} className={`hover:border-red-600 border-2 border-transparent px-4 py-2 rounded-lg cursor-pointer ${selected === '/students-info' && "bg-red-700 text-white"}`}>Students</Link>
                 <Link href="/teachers-info" onClick={() => setSelected("/teachers-info}")} className={`hover:border-red-600 border-2 border-transparent px-4 py-2 rounded-lg cursor-pointer ${selected === '/teachers-info' && "bg-red-700 text-white"}`}>Teachers</Link>
-                <Link href="" onClick={() => setSelected("About")} className={`hover:border-red-600 border-2 border-transparent px-4 py-2 rounded-lg cursor-pointer ${selected === 'About' && "bg-red-700 text-white"}`}>About</Link>
+                <Link href="/about" onClick={() => setSelected("/about")} className={`hover:border-red-600 border-2 border-transparent px-4 py-2 rounded-lg cursor-pointer ${selected === '/about' && "bg-red-700 text-white"}`}>About</Link>
             </div>
         </div>
     )
